86a2e701c2d1455563ed294175c672c5767f7b3a
saehyen <rlatpgus4564@naver.com>
2021-08-15 18:11:58 +0900

    자바스크립트_클래스

---
+++ JavaScript/DreamCoding/class.js
@@ -0,0 +1,135 @@
+'use strict';
+// Object-oriendted programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+//  - introduced in ES6
+//  - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+  // constructor
+  constructor(name, age) {
+    // fields
+    this.name = name;
+    this.age = age;
+  }
+
+  // methods
+  speak() {
+    console.log(`${this.name}: hello!`);
+  }
+}
+
+const KSH = new Person('KSH', 20);
+console.log(KSH.name);
+console.log(KSH.age);
+KSH.speak();
+
+// 2. Getter and setters
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+ // getter
+  get age() {
+    return this._age;
+  }
+ // 바보같이 -1을 입력하더라도 0으로 세팅해줌
+  set age(value) {
+    // if (value < 0) {
+    //   throw Error('age can not be negative');
+    // }
+    this._age = value < 0 ? 0 : value;
+  }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
+
+// 3. Fields (public, private)
+// Too soon!
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Class_fields
+class Experiment {
+  publicField = 2;
+  #privateField = 0;
+}
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+  static publisher = 'Dream Coding';
+  constructor(articleNumber) {
+    this.articleNumber = articleNumber;
+  }
+
+  static printPublisher() {
+    console.log(Article.publisher);
+  }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape {
+  constructor(width, height, color) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw() {
+    console.log(`drawing ${this.color} color!`);
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+  draw() {
+    super.draw();
+    console.log('🔺');
+  }
+  getArea() {
+    return (this.width * this.height) / 2;
+  }
+
+  toString() {
+    return `Triangle: color: ${this.color}`;
+  }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+// 상속받았는지 확인
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle); //T
+console.log(triangle instanceof Rectangle); //F
+console.log(triangle instanceof Triangle); //T
+console.log(triangle instanceof Shape); //T
+console.log(triangle instanceof Object); //T
+console.log(triangle.toString());
+
+let obj = { value: 5 };
+function change(value) {
+  value.value = 7;
+}
+change(obj);
+console.log(obj);
