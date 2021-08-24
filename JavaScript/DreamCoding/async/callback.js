fdd3dd788da85aa85b234ef2c205769129e03e62
saehyen <rlatpgus4564@naver.com>
2021-08-24 20:47:35 +0900

    자바스크립트_콜백

---
+++ JavaScript/DreamCoding/async/callback.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// JavaScript is synchronous.
+// 호이스팅이 된 이후부터 순서에 맞게 동기적으로 실행됨
+// Execute the code block in orger after hoisting.
+// hoisting: var, function declaration
+console.log('1');
+//setTimeoust : 지정한 시간이 지나면 callback함수 호출
+setTimeout(() => console.log('2'), 1000);
+console.log('3');
+
+// Synchronous callback
+function printImmediately(print) {
+  print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'), 2000);
+
+// Callback Hell example
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === 'ellie' && password === 'dream') ||
+        (id === 'coder' && password === 'academy')
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error('not found'));
+      }
+    }, 2000);
+  }
+
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === 'ellie') {
+        onSuccess({ name: 'ellie', role: 'admin' });
+      } else {
+        onError(new Error('no access'));
+      }
+    }, 1000);
+  }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+  id,
+  password,
+  user => {
+    userStorage.getRoles(
+      user,
+      userWithRole => {
+        alert(
+          `Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+        );
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  },
+  error => {
+    console.log(error);
+  }
+);
